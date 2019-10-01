bc89a85af731d30ca4a48962217bb0229090550e
jnadim19 <55325643+jnadim19@users.noreply.github.com>
2019-10-01 00:16:28 -0700

    Add files via upload

---
+++ string manipulation.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mystr As String
+        Dim reverse As String
+        Dim mychar As Char
+        Dim count As Integer
+
+
+        mystr = ""
+        reverse = ""
+        mychar = ""
+        count = 0
+
+
+        Console.WriteLine("Enter the value you wish to revese:")
+        mystr = Console.ReadLine()
+
+        For count = Len(mystr) To 1 Step -1
+            mychar = Mid(mystr, count)
+            reverse = reverse & mychar
+
+        Next
+        Console.WriteLine(reverse)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
