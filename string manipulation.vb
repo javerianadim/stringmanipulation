45494725494f24dbc8ad0952e40344583bbb82d8
javerianadim <55325643+javerianadim@users.noreply.github.com>
2019-10-22 19:22:44 +0500

    Update string manipulation.vb

--- string manipulation.vb
+++ string manipulation.vb
@@ -10,14 +10,14 @@
         reverse  = ""
         str = ""
         character = ""
-        count = 1
+        count = 0
 
 
         Console.WriteLine("Enter value you want to revese:")
         str = Console.ReadLine()
 
         For count = Len(str) To 1 Step -1
-            character = Mid(str, 1)
+        character = Mid(str, count , 1)
             reverse = reverse & character
 
         Next
